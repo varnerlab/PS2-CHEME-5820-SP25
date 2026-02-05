9a9110370bb5f8efa07a850ca3bef1a8403f0bae
Jeffrey Varner <jdv27@cornell.edu>
2026-02-05 14:15:35 -0500

    many changes

--- Include.jl
+++ Include.jl
@@ -3,6 +3,9 @@ const _ROOT = @__DIR__;
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 
+# did this file get called?
+const _DID_INCLUDE_FILE_GET_CALLED = true;
+
 # if we are missing any packages, install them -
 using Pkg;
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
